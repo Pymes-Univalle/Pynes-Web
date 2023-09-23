18ea0e175ca9998ce7d7a3f19af26403228d499c
Samuel <samuelaraoz789@gmail.com>
2023-09-23 18:39:14 -0400

    Imagenes y atributos

--- app/Producto/Crear/page.tsx
+++ app/Producto/Crear/page.tsx
@@ -1,248 +1,378 @@
-'use client'
-import { Button, Input, Select, SelectItem, Textarea, Image } from '@nextui-org/react';
-import React, { use, useEffect, useState } from 'react';
+"use client";
+import {
+  Button,
+  Input,
+  Select,
+  SelectItem,
+  Textarea,
+  Image,
+} from "@nextui-org/react";
+import React, { use, useEffect, useState } from "react";
 
-import axios from 'axios';
+import axios from "axios";
 
-import { url } from 'inspector';
+interface Producto {
+  nombre: string;
+  precio: string;
+  descripcion: string;
+  cantidad: string;
+  idCategoria: number;
+  idProductor: number;
+  fechaActualizacion: Date;
+}
+
+interface Atributos {
+  nombre: string;
+  valor: string;
+}
+
+interface Ruta {
+  ruta: string;
+  fechaActualizacion: Date;
+}
 
 export default function Page() {
-
-    const [image, setImage] = useState<File | undefined>(undefined);
-   
-    
-    const [imageUrl , setUrl] = useState<String>('')
-
-
-    const [idCategoria, setIdCategoria] = useState('');
-    const [categorias, setCategorias] = useState([]);
-    const [atributos, setAtributos] = useState<{ nombre: string; valor: string; }[]>([{ nombre: '', valor: '' }]);
-    const [imagePreviews, setImagePreviews] = useState<{ src: string; alt: string }[]>([]);
-
-    useEffect(() => {
-      // Realiza la solicitud a la API utilizando Axios
-      axios.get('/api/categoria')
-        .then((response) => {
-          // response.data debe contener la lista de categorías desde tu API
-          setCategorias(response.data.data);
-        })
-        .catch((error) => {
-          console.error('Error al cargar categorías desde la API:', error);
-        });
-    }, []); // Se ejecuta solo una vez al montar el componente
   
-    const handleCategoryChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
-      const selectedValue = event.target.value;
-      setIdCategoria(selectedValue);
-      console.log(selectedValue);
-    };
+  const [idCategoria, setIdCategoria] = useState("");
+  const [categorias, setCategorias] = useState([]);
+  const [atributos, setAtributos] = useState<
+    { nombre: string; valor: string }[]
+  >([{ nombre: "", valor: "" }]);
+  const [imagePreviews, setImagePreviews] = useState<{ src: string; alt: string; file: File }[]>([]);
 
-    
-
-    function handleFile(event: React.ChangeEvent<HTMLInputElement>): void {
-        const file = event.target.files?.[0];
-        setImage(file);
-        console.log(file);
-
-        if(file){
-          const url = URL.createObjectURL(file);
-          setUrl(url);
-          console.log("Esto es la " + url);
-        }
-    }
-
-    function createImagePreview(file: File): Promise<{ src: string; alt: string }> {
-      return new Promise((resolve) => {
-        const reader = new FileReader();
-        reader.onload = (event) => {
-          resolve({
-            src: event.target?.result as string,
-            alt: file.name,
-          });
-        };
-        reader.readAsDataURL(file);
+  useEffect(() => {
+    // Realiza la solicitud a la API utilizando Axios
+    axios
+      .get("/api/categoria")
+      .then((response) => {
+        // response.data debe contener la lista de categorías desde tu API
+        setCategorias(response.data.data);
+      })
+      .catch((error) => {
+        console.error("Error al cargar categorías desde la API:", error);
       });
-    }
-    
+  }, []); // Se ejecuta solo una vez al montar el componente
 
-    function sumbit(event: React.MouseEvent<HTMLButtonElement>): void {
-      console.log("CLick en el submit")
-      console.log("Imágenes a enviar:", imagePreviews);
-      console.log("Atributos:", atributos);
-      /*
-      if(!image){
-        console.log("Porfavor seleccione una imagen");
-        return;
+  const handleCategoryChange = (
+    event: React.ChangeEvent<HTMLSelectElement>
+  ) => {
+    const selectedValue = event.target.value;
+    setIdCategoria(selectedValue);
+  };
+
+  /*
+  function handleFile(event: React.ChangeEvent<HTMLInputElement>): void {
+      const file = event.target.files?.[0];
+      setImage(file);
+      console.log(file);
+
+      if(file){
+        const url = URL.createObjectURL(file);
+        setUrl(url);
+        console.log("Esto es la " + url);
       }
+  }*/
 
-      const data = new FormData();
+  function createImagePreview(
+    file: File
+  ): Promise<{ src: string; alt: string }> {
+    return new Promise((resolve) => {
+      const reader = new FileReader();
+      reader.onload = (event) => {
+        resolve({
+          src: event.target?.result as string,
+          alt: file.name,
+        });
+      };
+      reader.readAsDataURL(file);
+    });
+  }
 
-      data.append('file', image); // Correctly appends the image file
-      data.append('upload_preset', 'test_pymes');
-      data.append('cloud_name', 'di9vckxy5');
-      data.append('filename','hola');
-  
+  async function sumbit(
+    event: React.FormEvent<HTMLFormElement>
+  ): Promise<void> {
+    event.preventDefault();
+    const formElements = event.currentTarget.elements;
+    const nombre =
+      (formElements.namedItem("nombre") as HTMLInputElement)?.value || "";
+    const precio =
+      (formElements.namedItem("precio") as HTMLInputElement)?.value || "";
+    const descripcion =
+      (formElements.namedItem("descripcion") as HTMLInputElement)?.value || "";
+    const cantidad =
+      (formElements.namedItem("cantidad") as HTMLInputElement)?.value || "";
+    const atributosArray = atributos.map((atributo) => ({
+      nombre: atributo.nombre,
+      valor: atributo.valor,
+    }));
 
-      fetch('https://api.cloudinary.com/v1_1/di9vckxy5/image/upload',{
-        method: 'post',
-        body: data
-      }).then((response) => {
-        console.log("Se subio correctamente la imagen " + response)
+    console.log("CLick en el submit");
+    console.log("Imágenes a enviar:", imagePreviews);
+    console.log("Atributos:", atributos);
+    console.log("Id Categoria", idCategoria);
+    console.log(
+      "Nombre: ",
+      nombre,
+      " Precio: ",
+      precio,
+      " descripcion: ",
+      descripcion,
+      " cantidad: ",
+      cantidad
+    );
 
-      }).catch((errors) => {
-        console.log(errors)
-      })*/
+    // Crear un objeto FormData
+    const formData = new FormData();
 
-    }
+    // Adjuntar los datos del producto al FormData
+    formData.append("nombre", nombre);
+    formData.append("precio", precio);
+    formData.append("descripcion", descripcion);
+    formData.append("cantidad", cantidad);
+    formData.append("idCategoria", idCategoria);
+    formData.append("atributos", JSON.stringify(atributosArray));
+    //formData.append('fechaActualizacion', Date.now());
 
-    function removeImagePreview(index: number): void {
-      const updatedImagePreviews = [...imagePreviews];
-      updatedImagePreviews.splice(index, 1);
-      setImagePreviews(updatedImagePreviews);
-    }
+    // Adjuntar cada imagen al FormData con un nombre de campo distinto
+    imagePreviews.forEach((preview, index) => {
+      formData.append(`imagen_${index}`, preview.src);
+    });
 
-    async function handleFileChange(event: React.ChangeEvent<HTMLInputElement>): Promise<void> {
-      const files = event.target.files;
-    
-      if (files) {
-        const newImagePreviews = await Promise.all(Array.from(files).map(createImagePreview));
-        setImagePreviews([...imagePreviews, ...newImagePreviews]);
+    try {
+      // Realizar la solicitud a tu API utilizando axios
+      const response = await axios.post("/api/producto/", formData);
+
+      if (response.status === 200) {
+        console.log("Datos y imágenes enviados correctamente a la API");
+        const productId = response.data.message; // Aquí accedemos al campo del ID
+        console.log("El ID es:", productId);
         
+        // Aquí puedes redirigir o hacer otras acciones después del envío exitoso
+        await Promise.all(
+          imagePreviews.map(async (preview, index) => {
+            const data = new FormData();
+            console.log(preview.file.name);
+            data.append("file", preview.file);
+            data.append('upload_preset', 'test_pymes');
+            data.append('cloud_name', 'di9vckxy5');
+           
+
+            const cloudinaryResponse = await fetch(
+              "https://api.cloudinary.com/v1_1/di9vckxy5/image/upload",
+              {
+                method: "post",
+                body: data,
+              }
+            ).then((response) => {
+              console.log("Se subio correctamente la imagen " + response)
+        
+            }).catch((errors) => {
+              console.log(errors)
+            });
+/*
+            if (cloudinaryResponse.ok) {
+              console.log(`Imagen ${index + 1} subida a Cloudinary`);
+              const cloudinaryData = await cloudinaryResponse.json();
+              console.log("URL de la imagen:", cloudinaryData.url);
+            } else {
+              console.error(
+                `Error al subir la imagen ${index + 1} a Cloudinary`
+              );
+            }*/
+          })
+        );
+
+      } else {
+        console.error("Error al enviar los datos y las imágenes a la API");
       }
-    }
-    function handleAtributoChange(index: number, field: 'nombre' | 'valor', value: string): void {
-      const updatedAtributos = [...atributos];
-      updatedAtributos[index][field] = value;
-      setAtributos(updatedAtributos);
+    } catch (error) {
+      console.error("Error al realizar la solicitud a la API:", error);
     }
 
-    function addAtributo(): void {
-      setAtributos([...atributos, { nombre: '', valor: '' }]);
+    /*
+    if(!image){
+      console.log("Porfavor seleccione una imagen");
+      return;
     }
 
-    function removeAtributo(index: number): void {
-      const updatedAtributos = [...atributos];
-      updatedAtributos.splice(index, 1);
-      setAtributos(updatedAtributos);
-    }
+    const data = new FormData();
 
-    return (
+    data.append('file', image); // Correctly appends the image file
+    data.append('upload_preset', 'test_pymes');
+    data.append('cloud_name', 'di9vckxy5');
+    data.append('filename','hola');
+
+
+    fetch('https://api.cloudinary.com/v1_1/di9vckxy5/image/upload',{
+      method: 'post',
+      body: data
+    }).then((response) => {
+      console.log("Se subio correctamente la imagen " + response)
+
+    }).catch((errors) => {
+      console.log(errors)
+    })*/
+  }
+
+  function removeImagePreview(index: number): void {
+    const updatedImagePreviews = [...imagePreviews];
+    updatedImagePreviews.splice(index, 1);
+    setImagePreviews(updatedImagePreviews);
+  }
+
+  async function handleFileChange(event: React.ChangeEvent<HTMLInputElement>): Promise<void> {
+    const files = event.target.files;
+
+    if (files) {
+      const newImagePreviews = await Promise.all(
+        Array.from(files).map(async (file) => ({
+          src: URL.createObjectURL(file),
+          alt: file.name,
+          file,
+        }))
+      );
+      setImagePreviews([...imagePreviews, ...newImagePreviews]);
+    }
+  }
+
+  function handleAtributoChange(
+    index: number,
+    field: "nombre" | "valor",
+    value: string
+  ): void {
+    const updatedAtributos = [...atributos];
+    updatedAtributos[index][field] = value;
+    setAtributos(updatedAtributos);
+  }
+
+  function addAtributo(): void {
+    setAtributos([...atributos, { nombre: "", valor: "" }]);
+  }
+
+  function removeAtributo(index: number): void {
+    const updatedAtributos = [...atributos];
+    updatedAtributos.splice(index, 1);
+    setAtributos(updatedAtributos);
+  }
+
+  return (
     <>
-    
       <div className="bg-blanco min-h-screen text-black ">
         <div className="mx-auto max-w-5xl">
           <h1 className=" text-black text-2xl text-center font-bold mb-8 mt-5">
-          {" "}
-          Crear Producto{" "}
+            {" "}
+            Crear Producto{" "}
           </h1>
-          <form className='p-5 border-1 shadow'>
-           
+          <form
+            id="miFormulario"
+            method="post"
+            className="p-5 border-1 shadow"
+            onSubmit={sumbit}
+          >
             <div className="mb-5 mt-5">
-                <Input
-                  id="nombre"
-                  key="outside"
-                  type="text"
-                  label="Nombre"
-                  required
-                />
-              
+              <Input
+                id="nombre"
+                key="outside"
+                type="text"
+                label="Nombre"
+                required
+              />
             </div>
             <div className="mb-5">
-              <Input 
-                id="precio" 
-                key="outside" 
-                label="Precio" 
-                required 
-                
-                />
+              <Input id="precio" key="outside" label="Precio" required />
             </div>
             <div className="mb-5">
-              <Textarea 
-                id="descripcion" 
-                key="outside" 
-                label="Descripcion" 
-                required 
-                
-                />
+              <Textarea
+                id="descripcion"
+                key="outside"
+                label="Descripcion"
+                required
+              />
             </div>
             <div>
               <Select
-              
                 label="Categoría"
                 placeholder="Selecciona una categoría"
                 value={idCategoria}
-                onChange={ (selectedValue: any) => handleCategoryChange(selectedValue)  }
+                onChange={(selectedValue: any) =>
+                  handleCategoryChange(selectedValue)
+                }
                 className="mb-5 max-w-xs"
               >
                 {categorias.map((category) => (
-                  <SelectItem className='text-black' key={category['idCategoria']} value={category['idCategoria']}>
-                    {category['nombre']}
+                  <SelectItem
+                    className="text-black"
+                    key={category["idCategoria"]}
+                    value={category["idCategoria"]}
+                  >
+                    {category["nombre"]}
                   </SelectItem>
                 ))}
               </Select>
             </div>
 
             <div className="mb-5">
-              <Input 
-                id="cantidad" 
-                key="outside" 
-                label="Cantidad" 
-                required 
-                
-                />
+              <Input id="cantidad" key="outside" label="Cantidad" required />
             </div>
 
-           {/*<div className="mb-5">
+            {/*<div className="mb-5">
               <Input type="file" onChange={handleFile}  />
                 </div>*/}
 
             <div className="mb-5">
               <Input type="file" onChange={handleFileChange} multiple />
             </div>
-            
+
             <div className="mb-5">
               <h2>Imágenes seleccionadas:</h2>
               <div className="flex flex-wrap">
-              {imagePreviews.length > 0 && (
-              <div className="mb-5">
-                <h2>Imágenes seleccionadas:</h2>
-                <div className="flex flex-wrap">
-                  {imagePreviews.map((image, index) => (
-                    <div key={index} className="flex-shrink-0 mr-2">
-                      <Image
-                        src={image.src}
-                        alt={`Previsualización ${index + 1}`}
-                        width={200}
-                        height={200}
-                      />
-                      <button type="button" onClick={() => removeImagePreview(index)}>
-                        Eliminar
-                      </button>
+                {imagePreviews.length > 0 && (
+                  <div className="mb-5">
+                    <h2>Imágenes seleccionadas:</h2>
+                    <div className="flex flex-wrap">
+                      {imagePreviews.map((image, index) => (
+                        <div key={index} className="flex-shrink-0 mr-2">
+                          <Image
+                            src={image.src}
+                            alt={`Previsualización ${index + 1}`}
+                            width={200}
+                            height={200}
+                          />
+                          <button
+                            type="button"
+                            onClick={() => removeImagePreview(index)}
+                          >
+                            Eliminar
+                          </button>
+                        </div>
+                      ))}
                     </div>
-                  ))}
-                </div>
-              </div>
-            )}
-
+                  </div>
+                )}
               </div>
             </div>
 
-
             <div className="mb-5">
               <h2>Atributos:</h2>
+              
+              
               {atributos.map((atributo, index) => (
                 <div key={index} className="mb-2">
                   <Input
                     type="text"
-                    label="Nombre"
+                    label={`Atributo ${index + 1} - Nombre`}
                     value={atributo.nombre}
-                    onChange={(e) => handleAtributoChange(index, 'nombre', e.target.value)}
+                    onChange={(e) =>
+                      handleAtributoChange(index, "nombre", e.target.value)
+                    }
                   />
                   <Input
                     type="text"
-                    label="Valor"
+                    label={`Atributo ${index + 1} - Valor`}
                     value={atributo.valor}
-                    onChange={(e) => handleAtributoChange(index, 'valor', e.target.value)}
+                    onChange={(e) =>
+                      handleAtributoChange(index, "valor", e.target.value)
+                    }
                   />
                   <button type="button" onClick={() => removeAtributo(index)}>
                     Eliminar Atributo
@@ -253,17 +383,10 @@ export default function Page() {
                 Agregar Atributo
               </button>
             </div>
-            <Button onClick={sumbit}>
-                Enviar 
-            </Button>
+            <Button type="submit">Enviar</Button>
           </form>
-        
-           
-      
         </div>
-          
       </div>
-    
     </>
-    );
+  );
 }
--- app/api/producto/route.ts
+++ app/api/producto/route.ts
@@ -1,16 +1,56 @@
+// Importa tus modelos de Prisma
 import prisma from "@/lib/prisma";
 import { NextResponse } from "next/server";
 
 
 export async function POST(request: Request) {
-    try {
-       
-        
-        return NextResponse.json({  }, { status: 200 });
-      } catch (error) {
-        console.log(error);
-        if (error instanceof Error) {
-          return NextResponse.json({ message: error.message }, { status: 500 });
-        }
-      }
+  try {
+    const formData = await request.formData();
+
+    // Extraer los campos del formulario
+    const nombre = formData.get('nombre');
+    const precio = formData.get('precio');
+    const descripcion = formData.get('descripcion');
+    const cantidad = formData.get('cantidad');
+    const idCategoria = formData.get('idCategoria');
+
+    // Extraer los atributos del cuerpo de la solicitud como un JSON
+    const atributosJSON = formData.get('atributos');
+    const atributos = atributosJSON ? JSON.parse(atributosJSON as string) : [];
+
+    // Crear el producto en la base de datos
+    const nuevoProducto = await prisma.productos.create({
+      data: {
+        nombre: nombre as string,
+        precio: parseFloat(precio as string),
+        descripcion: descripcion as string,
+        cantidad: parseInt(cantidad as string),
+        idCategoria: parseInt(idCategoria as string),
+        idProductor: 2,
+        fechaActualizacion: new Date(new Date().toISOString()),
+      },
+    });
+
+    await Promise.all(
+      atributos.map(async (atributo: { nombre: string; valor: string }) => {
+        await prisma.atributo.create({
+          data: {
+            nombre: atributo.nombre,
+            valor: atributo.valor,
+            idProducto: nuevoProducto.idProductos, // Asociar el atributo con el producto recién creado
+          },
+        });
+      })
+    );
+
+    // Subir las imágenes a Cloudinary y guardar las rutas en la base de datos
+    // ...
+
+    return NextResponse.json({ message: nuevoProducto.idProductos }, { status: 200 });
+  } catch (error) {
+    console.error('Error al procesar la solicitud:', error);
+    if (error instanceof Error) {
+      return NextResponse.json({ message: error.message }, { status: 500 });
+    }
+  }
 }
