5958ae5b8cfd5dbf58c421192d0436afd4f37e97
Samuel1105 <samuelaraoz789@gmail.com>
2023-11-17 09:23:09 -0400

    BackUp Ultimo

--- app/Producto/Mostrar/page.tsx
+++ app/Producto/Mostrar/page.tsx
@@ -1,91 +1,286 @@
+// "use client";
+// import DeleteIcon from "@/DeleteIcon";
+// import EditIcon from "@/EditIcon";
+// import EyeIcon from "@/EyeIcon";
+// import {
+//   Button,
+//   Link,
+//   Pagination,
+//   Popover,
+//   PopoverContent,
+//   PopoverTrigger,
+//   Table,
+//   TableBody,
+//   TableCell,
+//   TableColumn,
+//   TableHeader,
+//   TableRow,
+// } from "@nextui-org/react";
+// import axios from "axios";
+// import { useRouter } from "next/navigation";
+// import React, { useEffect, useMemo, useState } from "react";
+
+// interface Producto {
+//   estado:number;
+//   fechaActualizacion: Date;
+// }
+
+// export default function Mostrar() {
+//   const router = useRouter();
+//   const [page, setPage] = useState(1);
+//   const rowsPerPage = 10;
+//   const [productosD, setProductor] = useState([]);
+
+//   const producto: Producto = {
+//     estado: 0,
+//     fechaActualizacion: new Date(new Date().toISOString()),
+//   };
+
+//   const Reload = () => {
+//     axios.get("/api/producto")
+//     .then((res) =>{
+//         if(res.data && res.data.data){
+//             setProductor(res.data.data);
+//         }
+//     })
+//     .catch((error) => {
+//         console.log("Error eal obtener los datos de la Api" + error);
+//     })
+//   }
+
+//   useEffect(() => {
+//     Reload();
+//   }, []);
+
+//   const pages = Math.ceil(productosD.length / rowsPerPage);
+//   const items = useMemo(() => {
+//     const start = (page - 1) * rowsPerPage;
+//     const end = start + rowsPerPage;
+//     return productosD.slice(start, end);
+//   }, [page, productosD]);
+
+//   const ClickDetalles = (id: any) => {
+//     router.push(`/Producto/Detalles?id=${id}`);
+//   };
+
+//   const ClickEditar = (id: any) => {
+//     router.push(`/Producto/Editar?id=${id}`);
+//   };
+//   const CrearProducto = () => {
+//     router.push("/Producto/Crear");
+//   };
+  
+//   const handleDeleteConfirm = async (id: any) => {
+//     try {
+//         await axios.delete(`/api/producto/${id}`, {
+//             data: {
+
+//                 estado: producto.estado,
+//             }
+//         });
+
+//        Reload();
+
+//     } catch (error) {
+//         console.log("Error en la solicitud PUT(Delete)" + error);
+//     }
+//   };
+
+//   return (
+//     <>
+//       <div className="text-black bg-blanco p-4">
+//         <h1 className="text-center text-2x1 mb-4"> Lista de Productos</h1>
+//         <Link
+//           onClick={CrearProducto}
+//           className="btn bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+//         >
+//           Crear Productos
+//         </Link>
+//         <Table
+//           aria-label="Example table with client side pagination"
+//           bottomContent={
+//             <div className="flex w-full justify-center">
+//               <Pagination
+//                 isCompact
+//                 showControls
+//                 showShadow
+//                 color="secondary"
+//                 page={page}
+//                 total={pages}
+//                 onChange={(page) => setPage(page)}
+//               />
+//             </div>
+//           }
+//           classNames={{
+//             wrapper: "min-h-[222px]",
+//           }}
+//         >
+//           <TableHeader>
+//             <TableColumn hidden  key="id">Id</TableColumn>
+//             <TableColumn key="nombre">Nombre</TableColumn>
+//             <TableColumn key="precio">Precio</TableColumn>
+//             <TableColumn key="cantidad">Cantidad</TableColumn>
+//             <TableColumn key="categoria">Categoria</TableColumn>
+
+//             <TableColumn key="ver">Ver</TableColumn>
+//             <TableColumn key="editar">Editar</TableColumn>
+//             <TableColumn key="eliminar">Eliminar</TableColumn>
+//           </TableHeader>
+//           <TableBody items={items}>
+//             {(item) => (
+//               <TableRow key={item["idProductos"]}>
+//                 <TableCell hidden>
+//                   <div  className="flex gap-4">{item["idProductos"]}</div>
+//                 </TableCell>
+//                 <TableCell>{item["nombre"]}</TableCell>
+//                 <TableCell>{item["precio"]}</TableCell>
+//                 <TableCell>{item["cantidad"]}</TableCell>
+//                 <TableCell>{item["categoria"]["nombre"]}</TableCell>
+
+//                 <TableCell>
+//                   <Button
+//                     className="flex items-center text-black hover:text-gray-800"
+//                     color="primary"
+//                     onClick={() => ClickDetalles(item["idProductos"])}
+//                   >
+//                     <EyeIcon className="w-6 h-6 text-black" />
+//                     Ver
+//                   </Button>
+//                 </TableCell>
+
+//                 <TableCell>
+//                   <Button
+//                     className="flex items-center text-black hover:text-gray-800"
+//                     color="success"
+//                     onClick={() => ClickEditar(item["idProductos"])}
+//                   >
+//                     <EditIcon className="w-6 h-6 text-black" />
+//                     Editar
+//                   </Button>
+//                 </TableCell>
+
+//                 <TableCell>
+//                   <Popover
+
+//                     showArrow
+//                     backdrop="opaque"
+//                     placement="right"
+//                     classNames={{
+//                         base: "py-3 px-4 border border-default-200 bg-gradient-to-br from-white to-default-300 dark:from-default-100 dark:to-default-50",
+//                         arrow: "bg-default-200",
+//                     }}
+//                     >
+//                     <PopoverTrigger>
+//                         <Button color="danger">
+//                         Eliminar
+//                         <DeleteIcon />
+//                         </Button>
+//                     </PopoverTrigger>
+//                     <PopoverContent>
+//                         {(titleProps) => (
+//                         <div className="px-1 py-2 w-full">
+//                             <p
+//                             className="text-small font-bold text-foreground"
+//                             {...titleProps}
+//                             >
+//                             ¿Estás seguro de querer eliminar a {item["nombre"]}?
+//                             </p>
+//                             <div className="mt-2 flex flex-col gap-2 w-full">
+//                             <Button color="success" onClick={ ()=> handleDeleteConfirm(item["idProductos"])    }>
+//                                 Confirmar
+//                             </Button>
+//                             <Button >Cancelar</Button>
+//                             </div>
+//                         </div>
+//                         )}
+//                     </PopoverContent>
+//                   </Popover>
+//                 </TableCell>
+//               </TableRow>
+//             )}
+//           </TableBody>
+//         </Table>
+//       </div>
+//     </>
+//   );
+// }
+
 "use client";
 import DeleteIcon from "@/DeleteIcon";
 import EditIcon from "@/EditIcon";
 import EyeIcon from "@/EyeIcon";
 import {
-  Button,
-  Link,
-  Pagination,
-  Popover,
-  PopoverContent,
-  PopoverTrigger,
-  Table,
-  TableBody,
-  TableCell,
-  TableColumn,
-  TableHeader,
-  TableRow,
+  Button, Link, Pagination, Popover, PopoverContent, PopoverTrigger,
+  Table, TableBody, TableCell, TableColumn, TableHeader, TableRow,
+  Card, CardBody, CardFooter, Image, Input
 } from "@nextui-org/react";
 import axios from "axios";
 import { useRouter } from "next/navigation";
 import React, { useEffect, useMemo, useState } from "react";
-
-interface Producto {
-  estado:number;
-  fechaActualizacion: Date;
-}
-
+ 
 export default function Mostrar() {
   const router = useRouter();
   const [page, setPage] = useState(1);
-  const rowsPerPage = 10;
-  const [productosD, setProductor] = useState([]);
-
-  const producto: Producto = {
-    estado: 0,
-    fechaActualizacion: new Date(new Date().toISOString()),
-  };
-
+  const rowsPerPage = 3;
+  const [productos, setProductos] = useState<any[]>([]);
+  const [searchTerm, setSearchTerm] = useState("");
+ 
   const Reload = () => {
     axios.get("/api/producto")
     .then((res) =>{
         if(res.data && res.data.data){
-            setProductor(res.data.data);
+          setProductos(res.data.data);
         }
     })
     .catch((error) => {
-        console.log("Error eal obtener los datos de la Api" + error);
+        console.log("Error al obtener los datos de la Api" + error);
     })
   }
-
+ 
   useEffect(() => {
     Reload();
   }, []);
-
-  const pages = Math.ceil(productosD.length / rowsPerPage);
+ 
+  const pages = Math.ceil(productos.length / rowsPerPage);
   const items = useMemo(() => {
     const start = (page - 1) * rowsPerPage;
     const end = start + rowsPerPage;
-    return productosD.slice(start, end);
-  }, [page, productosD]);
-
+    return productos.slice(start, end);
+  }, [page, productos]);
+ 
+  const filteredProductos = useMemo(() => {
+    return productos.filter((producto) =>
+      producto.nombre.toLowerCase().includes(searchTerm.toLowerCase())
+    );
+  }, [productos, searchTerm]);
+ 
   const ClickDetalles = (id: any) => {
     router.push(`/Producto/Detalles?id=${id}`);
   };
-
+ 
   const ClickEditar = (id: any) => {
     router.push(`/Producto/Editar?id=${id}`);
   };
   const CrearProducto = () => {
     router.push("/Producto/Crear");
   };
-  
-  const handleDeleteConfirm = async (id: any) => {
+ 
+  const handleDeleteConfirm = async (id: number) => {
     try {
         await axios.delete(`/api/producto/${id}`, {
             data: {
-
-                estado: producto.estado,
-            }
+              //estado: producto.estado,
+              estado: 0,
+            },
         });
-
+ 
        Reload();
-
+ 
     } catch (error) {
         console.log("Error en la solicitud PUT(Delete)" + error);
     }
   };
-
+ 
   return (
     <>
       <div className="text-black bg-blanco p-4">
@@ -96,110 +291,85 @@ export default function Mostrar() {
         >
           Crear Productos
         </Link>
-        <Table
-          aria-label="Example table with client side pagination"
-          bottomContent={
-            <div className="flex w-full justify-center">
-              <Pagination
-                isCompact
-                showControls
-                showShadow
-                color="secondary"
-                page={page}
-                total={pages}
-                onChange={(page) => setPage(page)}
-              />
-            </div>
-          }
-          classNames={{
-            wrapper: "min-h-[222px]",
-          }}
-        >
-          <TableHeader>
-            <TableColumn hidden  key="id">Id</TableColumn>
-            <TableColumn key="nombre">Nombre</TableColumn>
-            <TableColumn key="precio">Precio</TableColumn>
-            <TableColumn key="cantidad">Cantidad</TableColumn>
-            <TableColumn key="categoria">Categoria</TableColumn>
-
-            <TableColumn key="ver">Ver</TableColumn>
-            <TableColumn key="editar">Editar</TableColumn>
-            <TableColumn key="eliminar">Eliminar</TableColumn>
-          </TableHeader>
-          <TableBody items={items}>
-            {(item) => (
-              <TableRow key={item["idProductos"]}>
-                <TableCell hidden>
-                  <div  className="flex gap-4">{item["idProductos"]}</div>
-                </TableCell>
-                <TableCell>{item["nombre"]}</TableCell>
-                <TableCell>{item["precio"]}</TableCell>
-                <TableCell>{item["cantidad"]}</TableCell>
-                <TableCell>{item["categoria"]["nombre"]}</TableCell>
-
-                <TableCell>
-                  <Button
-                    className="flex items-center text-black hover:text-gray-800"
-                    color="primary"
-                    onClick={() => ClickDetalles(item["idProductos"])}
-                  >
-                    <EyeIcon className="w-6 h-6 text-black" />
+ 
+        <Input
+          placeholder="Buscar por nombre"
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+        />
+ 
+        <div className="gap-3 grid grid-cols-2 sm:grid-cols-4 mt-3">
+          {filteredProductos.map((item) => (
+            <Card
+              shadow="sm"
+              key={item.idProductos}
+            >
+              <CardBody className="overflow-visible p-0">
+                {item.ruta.map((rutaItem : any, index : number) => (
+                  <div key={index}>
+                    {item.mainIndex === index && (  // Compara mainIndex con la posición del array
+                      <Image
+                        shadow="sm"
+                        radius="lg"
+                        width="100%"
+                        alt={`Image ${item.mainIndex}`}
+                        className="w-full object-cover h-[160px]"
+                        src={rutaItem.ruta}
+                      />
+                    )}
+                  </div>
+                ))}
+              </CardBody>
+              <CardFooter className="text-small justify-between items-center">
+                {/* <b>{item.nombre}</b> */}
+                {/* <p className="text-default-500">{item.precio}</p> */}
+                <Button className="text-tiny" color="primary" radius="full" size="sm"
+                  onClick={() => ClickDetalles(item["idProductos"])}>
+                  <EyeIcon className="w-6 h-6 text-white" />
                     Ver
-                  </Button>
-                </TableCell>
-
-                <TableCell>
-                  <Button
-                    className="flex items-center text-black hover:text-gray-800"
-                    color="success"
-                    onClick={() => ClickEditar(item["idProductos"])}
-                  >
-                    <EditIcon className="w-6 h-6 text-black" />
+                </Button>
+                <Button className="text-tiny" color="success" radius="full" size="sm"
+                  onClick={() => ClickEditar(item["idProductos"])}>
+                  <EditIcon className="w-6 h-6 text-white" />
                     Editar
-                  </Button>
-                </TableCell>
-
-                <TableCell>
-                  <Popover
-
-                    showArrow
-                    backdrop="opaque"
-                    placement="right"
-                    classNames={{
-                        base: "py-3 px-4 border border-default-200 bg-gradient-to-br from-white to-default-300 dark:from-default-100 dark:to-default-50",
-                        arrow: "bg-default-200",
-                    }}
-                    >
-                    <PopoverTrigger>
-                        <Button color="danger">
+                </Button>
+ 
+                <Popover showArrow
+                  backdrop="opaque"
+                  placement="right"
+                  classNames={{
+                      base: "py-3 px-4 border border-default-200 bg-gradient-to-br from-white to-default-300 dark:from-default-100 dark:to-default-50",
+                      arrow: "bg-default-200",
+                  }}>
+                  <PopoverTrigger>
+                    <Button className="text-tiny" color="danger" radius="full" size="sm">
+                      <DeleteIcon className="w-6 h-6 text-white" />
                         Eliminar
-                        <DeleteIcon />
+                    </Button>
+                  </PopoverTrigger>
+                  <PopoverContent>
+                    {(titleProps) => (
+                      <div className="px-1 py-2 w-full">
+                        <p
+                        className="text-small font-bold text-foreground"
+                        {...titleProps}
+                        >
+                        ¿Estás seguro de querer eliminar a {item["nombre"]}?
+                        </p>
+                        <div className="mt-2 flex flex-col gap-2 w-full">
+                        <Button color="success" onClick={() => handleDeleteConfirm(item["idProductos"])}>
+                            Confirmar
                         </Button>
-                    </PopoverTrigger>
-                    <PopoverContent>
-                        {(titleProps) => (
-                        <div className="px-1 py-2 w-full">
-                            <p
-                            className="text-small font-bold text-foreground"
-                            {...titleProps}
-                            >
-                            ¿Estás seguro de querer eliminar a {item["nombre"]}?
-                            </p>
-                            <div className="mt-2 flex flex-col gap-2 w-full">
-                            <Button color="success" onClick={ ()=> handleDeleteConfirm(item["idProductos"])    }>
-                                Confirmar
-                            </Button>
-                            <Button >Cancelar</Button>
-                            </div>
                         </div>
-                        )}
-                    </PopoverContent>
-                  </Popover>
-                </TableCell>
-              </TableRow>
-            )}
-          </TableBody>
-        </Table>
+                      </div>
+                    )}
+                  </PopoverContent>
+                </Popover>        
+              </CardFooter>
+            </Card>
+          ))}
+        </div>
+ 
       </div>
     </>
   );
--- app/api/producto/[id]/route.ts
+++ app/api/producto/[id]/route.ts
@@ -1,3 +1,229 @@
+// // Importa tus modelos de Prisma
+// import prisma from "@/lib/prisma";
+// import { NextResponse } from "next/server";
+
+// interface Params{
+//     params: {id:string};
+// }
+
+// // export async function GET(request:Request, {params}: Params) {
+// //     try {
+        
+// //         console.log(params.id)
+    
+// //         const productos = await prisma.productos.findFirst({
+// //             where:{
+// //                 idProductos: Number(params.id)
+                
+// //             },
+// //             include:{
+// //                 ruta:true,
+// //                 categoria: true,
+// //                 atributo: true
+// //               }
+// //         })
+
+// //         return NextResponse.json({
+// //             productos
+// //         })
+// //       } catch (error) {
+// //         console.log(error);
+// //         if (error instanceof Error) {
+// //           return NextResponse.json({ message: error.message }, { status: 500 });
+// //         }
+// //       }
+// // }
+
+// export async function GET(request:Request, {params}: Params) {
+//   try {
+      
+//     //console.log(params.id)
+//     const productoId = Number(params.id);
+//     const productos = await prisma.productos.findFirst({
+//       where:{
+//         idProductos: productoId,
+          
+//       },
+//       include:{
+//         ruta:true,
+//         categoria: true,
+//         atributo: true,
+//       },
+//     });
+
+//     if (!productos) {
+//       return NextResponse.json({ message: 'Producto no encontrado' }, { status: 404 });
+//     }
+
+//     return NextResponse.json({productos})
+
+//   } catch (error) {
+//     console.log(error);
+//     if (error instanceof Error) {
+//       return NextResponse.json({ message: error.message }, { status: 500 });
+//     }
+//   }
+// }
+
+// export async function PUT(request: Request, {params}: Params) {
+//   try {
+//     const formData = await request.formData();
+//     const productoId = Number(params.id); // Identificador del producto a actualizar
+
+//     // Procesa los campos a actualizar, por ejemplo: nombre, precio, descripción, etc.
+//     const nombre = formData.get('nombre');
+//     const precio = formData.get('precio');
+//     const descripcion = formData.get('descripcion');
+//     const cantidad = formData.get('cantidad');
+//     const idCategoria = formData.get('idCategoria');
+//     const idProductor = formData.get('idProductor');
+//     const fechaVencimientoValue = formData.get('fechaVencimineto');
+//     let fechaVencimiento: Date | undefined;
+
+//     if (fechaVencimientoValue && typeof fechaVencimientoValue === 'string') {
+//       // Convertir la cadena de fecha a un objeto de fecha
+//       fechaVencimiento = new Date(fechaVencimientoValue);
+//     }
+
+//     const atributosJSON = formData.get('atributos');
+//     const atributos = atributosJSON ? JSON.parse(atributosJSON as string) : [];    
+
+//     const productoActualizado = await prisma.productos.update({
+//       where: { idProductos: productoId },
+//       data: {
+//         nombre: nombre as string,
+//         precio: parseFloat(precio as string),
+//         descripcion: descripcion as string,
+//         cantidad: parseInt(cantidad as string),
+//         idCategoria: parseInt(idCategoria as string),
+//         idProductor: parseInt(idProductor as string),
+//         fechaActualizacion: new Date(new Date().toISOString()),
+//         fechaVencimiento: fechaVencimiento || null
+//       },
+//     });
+
+//     await Promise.all(
+//       atributos.map(async (atributo: { idAtributo: number, nombre: string; valor: string }) => {
+//         const existingAtributo = await prisma.atributo.findFirst({
+//           // Buscar un atributo con el mismo ID o nombre
+//           where: {
+//             OR: [
+//               { idAtributo: atributo.idAtributo },
+//               { nombre: atributo.nombre },
+//             ],
+//           },
+//         });
+
+//         if (existingAtributo) {
+//           // Si existe un atributo con el mismo ID o nombre, actualízalo
+//           const updatedAtributo = await prisma.atributo.update({
+//             where: {
+//               idAtributo: existingAtributo.idAtributo,
+//             },
+//             data: {
+//               nombre: atributo.nombre,
+//               valor: atributo.valor,
+//             },
+//           });
+//         }
+//         if (!existingAtributo) {
+//           // Si no existe un atributo con el mismo ID o nombre, créalo
+//           const newAtributo = await prisma.atributo.create({
+//             data: {
+//               nombre: atributo.nombre,
+//               valor: atributo.valor,
+//               idProducto: productoActualizado.idProductos,
+//             },
+//           });
+//         }
+//       })
+//     );
+
+//     // Procesar imágenes
+//     const urls: string[] = [];
+
+//     for (let index = 0; ; index++) {
+//       const fieldName = `imagen_${index}`;
+//       const imageData = formData.get(fieldName);
+
+//       if (!imageData) {
+//         // Si no se encuentra más información bajo ese campo, sal del bucle
+//         break;
+//       }
+
+//       // Convertir la imagen a un formato que puedas usar, como Blob
+//       const imageBlob = new Blob([imageData as unknown as Buffer], { type: 'image/jpeg' });
+
+//       // Crear un nuevo FormData para la imagen y agregarla a él
+//       const imageFormData = new FormData();
+//       imageFormData.append('file', imageBlob, 'image.jpg');
+//       imageFormData.append('upload_preset', 'test_pymes');
+
+//       // Subir la imagen a Cloudinary (o realizar la acción deseada con la imagen)
+//       const response = await fetch(
+//         'https://api.cloudinary.com/v1_1/di9vckxy5/image/upload',
+//         {
+//           method: 'POST',
+//           body: imageFormData,
+//         }
+//       );
+
+//       if (response.ok) {
+//         const responseData = await response.json() as { secure_url: string };
+//         const imageUrl = responseData.secure_url;
+//         urls.push(imageUrl);
+//         console.log("Imagen subida a Cloudinary:", imageUrl);
+//       }
+//     }
+
+//     // Guardar las URL de las imágenes en la base de datos
+//     await Promise.all(
+//       urls.map(async (url) => {
+//         await prisma.ruta.create({
+//           data: {
+//             ruta: url,
+//             idProducto: productoActualizado.idProductos,
+//             fechaActualizacion: new Date().toISOString(),
+//           },
+//         });
+//       })
+//     );
+
+//     return NextResponse.json({ message: 'Producto actualizado con éxito' }, { status: 200 });
+//   } catch (error) {
+//     console.error('Error al procesar la solicitud:', error);
+//     if (error instanceof Error) {
+//       return NextResponse.json({ message: error.message }, { status: 500 });
+//     }
+//   }
+// }
+
+// export async function DELETE(request: Request, { params }: Params) {
+//   try {
+//     const { estado } = await request.json();
+
+//     const updateProducto = await prisma.productos.update({
+//         where: {
+//             idProductos: Number(params.id),
+//         },
+//         data: {
+//             estado:estado,
+//             fechaActualizacion: new Date(new Date().toISOString())
+//         },
+//     });
+
+//     return NextResponse.json({
+//       updateProducto,
+//     });
+
+//   } catch (error) {
+//     console.log(error);
+//     if (error instanceof Error) {
+//       return NextResponse.json({ message: error.message }, { status: 500 });
+//     }
+//   }
+// }
+
 // Importa tus modelos de Prisma
 import prisma from "@/lib/prisma";
 import { NextResponse } from "next/server";
